9217187525543dceb56e7996f08ec15d6357004d
Douglas Campbell <dacampbelltbone@gmail.com>
2017-01-25 02:14:20 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './github/Profile.jsx';
+import Search from './github/Search.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'campbelld84',
+      userData: [],
+      userRepos: [],
+      perPage: 10
+    }
+  }
+
+//Get user data from github
+getUserData() {
+  $.ajax({
+    url:'https://api.github.com/users/'+this.state.username+'?client_id='+this.props.clientId+'&client_secret'+this.props.clientSecret,
+    dataType: 'json',
+    cache: false,
+    success: function(data) {
+      this.setState({userData: data});
+    }.bind(this),
+    error: function(xhr, status, err) {
+      this.setState({username: null});
+      alert(err);
+    }.bind(this)
+  });
+}
+
+//Get user repos
+getUserRepos() {
+  $.ajax({
+    url:'https://api.github.com/users/'+this.state.username+'/repos?per_page='+this.state.perPage +'&client_id='+this.props.clientId+'&client_secret'+this.props.clientSecret+'&sort=created',
+    dataType: 'json',
+    cache: false,
+    success: function(data) {
+      this.setState({userRepos: data});
+    }.bind(this),
+    error: function(xhr, status, err) {
+      this.setState({username: null});
+      alert(err);
+    }.bind(this)
+  });
+}
+
+handleFormSubmit(username) {
+  this.setState({username: username}, function() {
+    this.getUserData();
+    this.getUserRepos();
+  });
+}
+
+componentDidMount() {
+  this.getUserData();
+  this.getUserRepos();
+}
+
+  render() {
+    return(
+      <div>
+        <Search onFormSubmit = {this.handleFormSubmit.bind(this)}/>
+        <Profile {...this.state} />
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  clientId : React.PropTypes.string,
+  clientSecret: React.PropTypes.string
+}
+App.defaultProps = {
+  clientId: '9212da86aba07c3bef3c',
+  clientSecret: 'd7240d45122d5a6d239f5917d9a77e004e2699d9'
+}
+
+export default App
